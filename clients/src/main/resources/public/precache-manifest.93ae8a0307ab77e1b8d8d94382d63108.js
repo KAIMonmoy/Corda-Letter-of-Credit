b78d326d7d8ff5c4167270fc506508d2a69af4f1
KAI_Monmoy <aftarulislam@gmail.com>
2020-08-15 02:39:39 +0600

    :sparkles: Added Client App

---
+++ clients/src/main/resources/public/precache-manifest.93ae8a0307ab77e1b8d8d94382d63108.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "be857fa9a8fd3877426805e1f0fc447d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a293b929e94ffd87ba",
+    "url": "/static/css/main.1aa2d3f8.chunk.css"
+  },
+  {
+    "revision": "f22fc2bd0d1ff31b27b7",
+    "url": "/static/js/2.802316a2.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.802316a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89a293b929e94ffd87ba",
+    "url": "/static/js/main.24ffc163.chunk.js"
+  },
+  {
+    "revision": "8ca68dcd795ab470d0a6",
+    "url": "/static/js/runtime-main.2d3e9065.js"
+  },
+  {
+    "revision": "9c8a627169c185e6fcc4ba7646316042",
+    "url": "/static/media/ship.9c8a6271.png"
+  }
+]);
